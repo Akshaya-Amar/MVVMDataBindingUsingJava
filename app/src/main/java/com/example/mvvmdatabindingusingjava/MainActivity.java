e9ffecbcd7207918bef47831c0f360663a7af2ab
akshayaamar <akshayaamar05@gmail.com>
2024-01-21 23:50:13 +0530

    Code format

--- app/src/main/java/com/example/mvvmdatabindingusingjava/MainActivity.java
+++ app/src/main/java/com/example/mvvmdatabindingusingjava/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         MyViewModel viewModel = new ViewModelProvider(this).get(MyViewModel.class);
